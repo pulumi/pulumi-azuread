fdddfeeb9ae56df1188f1e44b02cb942c58da44c
Thomas Kappler <tkappler@pulumi.com>
2023-11-29 15:29:55 +0100

    Enable and test OIDC support (#580)
    The dependencies this provider uses to authenticate to Azure already support OIDC. This PR enables the configuration, and adds an e2e test that authenticates to Azure via OIDC.
    
    Also, it fixes an unrelated old bug I spotted where the wrong config key was used for MSI configuration.

--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -1,9 +1,11 @@
 // Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+//go:build nodejs || all
 // +build nodejs all
 
 package examples
 
 import (
+	"os"
 	"path"
 	"testing"
 
@@ -30,6 +32,29 @@ func TestSimple(t *testing.T) {
 	test := getJSBaseOptions(t).
 		With(integration.ProgramTestOptions{
 			Dir:           path.Join(getCwd(t), "simple"),
+			RunUpdateTest: true,
+			Secrets: map[string]string{
+				"password": "SecretP@sswd99!",
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+// The same test than the above, but authenticating via OIDC.
+func TestSimple_OIDC(t *testing.T) {
+	if os.Getenv("RUN_OIDC_TESTS") != "true" {
+		t.Skip("Skipping OIDC test without OIDC_ARM_CLIENT_ID")
+	}
+
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "simple"),
+			Env: []string{
+				"ARM_USE_OIDC=true",
+				// not strictly necessary but making sure we test the OIDC path
+				"ARM_CLIENT_SECRET=",
+			},
 			RunUpdateTest: true,
 			Secrets: map[string]string{
 				"password": "SecretP@sswd99!",
