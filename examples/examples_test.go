746d1ba8c98cea5ee5739103b4c5b3daaae92e32
Anton Tayanovskyy <anton@pulumi.com>
2023-12-27 13:20:53 -0500

    Stricter defaults

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -28,7 +28,5 @@ func getCwd(t *testing.T) string {
 }
 
 func getBaseOptions() integration.ProgramTestOptions {
-	return integration.ProgramTestOptions{
-		ExpectRefreshChanges: true,
-	}
+	return integration.ProgramTestOptions{}
 }
