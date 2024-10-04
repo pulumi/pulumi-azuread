89cda9e314485806b4a36cb00773ea06f93872f2
Daniel Bradley <daniel@pulumi.com>
2024-10-04 13:24:28 +0100

    Update internal version to 6

--- examples/go.mod
+++ examples/go.mod
@@ -1,4 +1,4 @@
-module github.com/pulumi/pulumi-azuread/examples/v5
+module github.com/pulumi/pulumi-azuread/examples/v6
 
 go 1.21
 
--- provider/cmd/pulumi-tfgen-azuread/main.go
+++ provider/cmd/pulumi-tfgen-azuread/main.go
@@ -17,8 +17,8 @@ package main
 import (
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfgen"
 
-	azuread "github.com/pulumi/pulumi-azuread/provider/v5"
-	"github.com/pulumi/pulumi-azuread/provider/v5/pkg/version"
+	azuread "github.com/pulumi/pulumi-azuread/provider/v6"
+	"github.com/pulumi/pulumi-azuread/provider/v6/pkg/version"
 )
 
 func main() {
