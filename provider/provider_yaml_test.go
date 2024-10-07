48f78139a1ed71843e4d9a48ec15b921543a532d
Daniel Bradley <daniel@pulumi.com>
2024-10-07 11:53:29 +0100

    Add AD App upgrade test

---
+++ provider/provider_yaml_test.go
@@ -0,0 +1,62 @@
+// Copyright 2016-2024, Pulumi Corporation.  All rights reserved.
+
+//go:build !go && !nodejs && !python && !dotnet
+// +build !go,!nodejs,!python,!dotnet
+
+package provider
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	_ "embed"
+
+	"github.com/pulumi/providertest"
+	"github.com/pulumi/providertest/optproviderupgrade"
+	"github.com/pulumi/providertest/providers"
+	"github.com/pulumi/providertest/pulumitest"
+	"github.com/pulumi/providertest/pulumitest/assertpreview"
+	"github.com/pulumi/providertest/pulumitest/opttest"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
+	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
+
+	"github.com/pulumi/pulumi-azuread/provider/v6/pkg/version"
+)
+
+func Test_ad_app(t *testing.T) {
+	test(t, filepath.Join("test-programs", "ad-app"))
+}
+
+func TestUpgradeCoverage(t *testing.T) {
+	providertest.ReportUpgradeCoverage(t)
+}
+
+func test(t *testing.T, dir string, opts ...optproviderupgrade.PreviewProviderUpgradeOpt) {
+	t.Helper()
+	if testing.Short() {
+		t.Skipf("Skipping in testing.Short() mode, assuming this is a CI run without cloud credentials")
+		return
+	}
+	rpFactory := providers.ResourceProviderFactory(providerServer)
+	cacheDir := providertest.GetUpgradeCacheDir(filepath.Base(dir), "5.53.5")
+	pt := pulumitest.NewPulumiTest(t, dir,
+		opttest.AttachProvider("azuread",
+			rpFactory.ReplayInvokes(filepath.Join(cacheDir, "grpc.json"), false /* allowLiveFallback */)))
+	previewResult := providertest.PreviewProviderUpgrade(t, pt, "azuread", "5.53.5", opts...)
+	assertpreview.HasNoReplacements(t, previewResult)
+	assertpreview.HasNoDeletes(t, previewResult)
+}
+
+// Use the non-embedded schema to avoid having to run generation before running the tests.
+//
+//go:embed cmd/pulumi-resource-azuread/schema.json
+var schemaBytes []byte
+
+func providerServer(_ providers.PulumiTest) (pulumirpc.ResourceProviderServer, error) {
+	ctx := context.Background()
+	version.Version = "0.0.1"
+	info := Provider()
+
+	return tfbridge.NewProvider(ctx, nil, "azuread", version.Version, info.P, info, schemaBytes), nil
+}
